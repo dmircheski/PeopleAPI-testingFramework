b3f8a37bde3522d515fc7700f3c5c72be32ea0fb
dmircheski <deni.mirceski@t.mk>
2021-09-10 19:38:09 +0200

    initial commit

---
+++ src/test/java/code/academy/peopleapi/InitialTestFile.java
@@ -0,0 +1,17 @@
+package code.academy.peopleapi;
+
+
+import org.testng.annotations.Test;
+
+public class InitialTestFile {
+
+    @Test
+    public void initialTest() {
+        // nekakov REQUEST do PEOPLE API
+
+        // People API mi vrakja nekakov response
+
+        // nekakva proverka na bodyto shto go ima vo toj response
+
+    }
+}
