f2a729bfcbd5a85a2277c2fa3cc47b6a90deaefd
dmircheski <deni.mirceski@t.mk>
2021-09-11 09:30:34 +0200

    day1 finished

--- src/test/java/code/academy/peopleapi/InitialTestFile.java
+++ src/test/java/code/academy/peopleapi/InitialTestFile.java
@@ -1,17 +1,29 @@
 package code.academy.peopleapi;
 
 
+import code.academy.client.PeopleApiClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
+
+
 
 public class InitialTestFile {
 
+    PeopleApiClient peopleApiClient = new PeopleApiClient();
+    HttpResponse response;
+
     @Test
-    public void initialTest() {
+    public void initialTest() throws Exception {
         // nekakov REQUEST do PEOPLE API
+        response = peopleApiClient.getWelcomeRequest();
+
+        String body = EntityUtils.toString(response.getEntity());
 
         // People API mi vrakja nekakov response
 
         // nekakva proverka na bodyto shto go ima vo toj response
+        // body === "message": "Welcome to People API"
 
     }
 }
