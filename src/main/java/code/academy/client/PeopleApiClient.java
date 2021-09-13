212b3f320a97f9f53d4c7ba1449fb741876c4f00
dmircheski <deni.mirceski@t.mk>
2021-09-13 19:48:58 +0200

    automation_day2 finished

--- src/main/java/code/academy/client/PeopleApiClient.java
+++ src/main/java/code/academy/client/PeopleApiClient.java
@@ -5,7 +5,9 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
@@ -13,6 +15,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 import javax.net.ssl.SSLContext;
 
@@ -91,4 +94,63 @@ public class PeopleApiClient {
 
         return response;
     }
+
+    public HttpResponse deleteOnePerson() throws Exception {
+        Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+
+        SSLContext sslContext = SSLContextBuilder
+                .create()
+                .loadTrustMaterial(new TrustSelfSignedStrategy())
+                .build();
+
+        HttpDelete request = new HttpDelete("https://people-api1.herokuapp.com/api/person/613e004966bb7645d800f41d");
+        request.setHeader(contentType);
+
+        HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
+
+        HttpResponse response = httpClient.execute(request);
+
+        HttpEntity entity = response.getEntity();
+        String body = EntityUtils.toString(response.getEntity());
+
+        HttpEntity newEntity = new StringEntity(body, ContentType.get(entity));
+        response.setEntity(newEntity);
+
+        return response;
+    }
+
+    public HttpResponse postNewPerson() throws Exception {
+        Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+
+        SSLContext sslContext = SSLContextBuilder
+                .create()
+                .loadTrustMaterial(new TrustSelfSignedStrategy())
+                .build();
+
+        HttpPost request = new HttpPost("https://people-api1.herokuapp.com/api/person");
+
+        JSONObject payloadAsObject = new JSONObject();
+        payloadAsObject.put("name","Pero");
+        payloadAsObject.put("surname","Blazevski");
+        payloadAsObject.put("age", 56);
+        payloadAsObject.put("isEmployed", true);
+        payloadAsObject.put("location","Skopje");
+
+        request.setHeader(contentType);
+        request.setEntity(new StringEntity(payloadAsObject.toString()));
+
+        HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
+
+        HttpResponse response = httpClient.execute(request);
+
+        HttpEntity entity = response.getEntity();
+        String body = EntityUtils.toString(response.getEntity());
+
+        HttpEntity newEntity = new StringEntity(body, ContentType.get(entity));
+        response.setEntity(newEntity);
+
+        return response;
+    }
+
+    // PUT method za domashna 
 }
